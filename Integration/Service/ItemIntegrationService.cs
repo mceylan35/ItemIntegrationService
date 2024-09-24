5641b96e7bcd8912743e42f43f9613bf77ad5d0d
mehmet.ceylan <mehmet.ceylan@ext.kocsistem.com.tr>
2024-09-24 23:05:28 +0300

    add SaveItem method ConcurrentDictionary Control

--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -1,5 +1,6 @@
 ﻿using Integration.Common;
 using Integration.Backend;
+using System.Collections.Concurrent;
 
 namespace Integration.Service;
 
@@ -7,6 +8,8 @@ public sealed class ItemIntegrationService
 {
     //This is a dependency that is normally fulfilled externally.
     private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
+    private readonly ConcurrentDictionary<string, object> _currentlyProcessing = new ConcurrentDictionary<string, object>();
+
 
     // This is called externally and can be called multithreaded, in parallel.
     // More than one item with the same content should not be saved. However,
@@ -14,15 +17,28 @@ public sealed class ItemIntegrationService
     // be allowed for performance reasons.
     public Result SaveItem(string itemContent)
     {
-        // Check the backend to see if the content is already saved.
-        if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+
+        try
         {
-            return new Result(false, $"Duplicate item received with content {itemContent}.");
+            if (!_currentlyProcessing.TryAdd(itemContent, null))
+            {
+                return new Result(false, $"'{itemContent}' içerik kaydedilmeyecek.");
+            }
+
+            // Check the backend to see if the content is already saved.
+            if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+            {
+                return new Result(false, $"Duplicate item received with content {itemContent}.");
+            }
+
+            var item = ItemIntegrationBackend.SaveItem(itemContent);
+
+            return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
         }
-
-        var item = ItemIntegrationBackend.SaveItem(itemContent);
-
-        return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+        finally  
+        {
+            _currentlyProcessing.TryRemove(itemContent, out _);
+        }
     }
 
     public List<Item> GetAllItems()
