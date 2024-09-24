ccce4427eace1d6251774d4bac7e8a9357f3d324
mehmet.ceylan <mehmet.ceylan@ext.kocsistem.com.tr>
2024-09-24 23:10:18 +0300

    add DistributedItemIntegrationService

--- Integration/Program.cs
+++ Integration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Integration.Service;
+using StackExchange.Redis;
 
 namespace Integration;
 
@@ -23,7 +24,35 @@ public abstract class Program
         Console.WriteLine("Everything recorded:");
 
         service.GetAllItems().ForEach(Console.WriteLine);
+         
+
+        ///DistributedItemIntegrationService
+
+        var redisConnection =   ConnectionMultiplexer.ConnectAsync("localhost:6379").Result;
+        var redisLockService = new RedisLockService(redisConnection);
+
+      
+        var distributedItemIntegrationService = new DistributedItemIntegrationService(redisLockService);
+         
+        ThreadPool.QueueUserWorkItem(async _ => Console.WriteLine(await distributedItemIntegrationService.SaveItemAsync("a")));
+        ThreadPool.QueueUserWorkItem(async _ => Console.WriteLine(await distributedItemIntegrationService.SaveItemAsync("b")));
+        ThreadPool.QueueUserWorkItem(async _ => Console.WriteLine(await distributedItemIntegrationService.SaveItemAsync("c")));
+
+        Thread.Sleep(500);
+         
+        ThreadPool.QueueUserWorkItem(async _ => Console.WriteLine(await distributedItemIntegrationService.SaveItemAsync("a")));
+        ThreadPool.QueueUserWorkItem(async _ => Console.WriteLine(await distributedItemIntegrationService.SaveItemAsync("b")));
+        ThreadPool.QueueUserWorkItem(async _ => Console.WriteLine(await distributedItemIntegrationService.SaveItemAsync("c")));
+
+        // İşlemlerin tamamlanması için yeterli süre
+        Thread.Sleep(5000);
+
+        Console.WriteLine("Everything recorded:");
+
+        // Kayıtlı tüm öğeleri listele
+        service.GetAllItems().ForEach(Console.WriteLine);
 
         Console.ReadLine();
     }
+}
 }
---
+++ Integration/Service/DistributedItemIntegrationService.cs
@@ -0,0 +1,56 @@
+﻿using Integration.Backend;
+using Integration.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration.Service
+{
+    public class DistributedItemIntegrationService
+    {
+        private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
+        private readonly RedisLockService _redisLockService;
+        private const string RedisKeyPrefix = "item-lock:";   
+
+        public DistributedItemIntegrationService(RedisLockService redisLockService)
+        {
+            _redisLockService = redisLockService;
+        }
+
+        public async Task<Result> SaveItemAsync(string itemContent)
+        {
+            string lockKey = $"{RedisKeyPrefix}{itemContent}";
+
+            try
+            {
+
+                if (!await _redisLockService.AcquireLockAsync(lockKey))
+                {
+                    return new Result(false, $"'{itemContent}' şu anda farklı bir servis tarafından işleniyor.");
+                }
+
+              
+                if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+                {
+                    return new Result(false, $"Aynı içerik tekrar alındı: {itemContent}.");
+                }
+
+               
+                var item = ItemIntegrationBackend.SaveItem(itemContent);
+                return new Result(true, $"İçerik kaydedildi: {itemContent}, id: {item.Id}");
+            }
+            finally
+            {
+                // Redisten sil
+                await _redisLockService.ReleaseLockAsync(lockKey);
+            }
+        }
+
+        public List<Item> GetAllItems()
+        {
+            return ItemIntegrationBackend.GetAllItems();
+        }
+    }
+}
